1861e37dd54234aaaea9b6577c62e32ecac9253b
Phil G <fillupgalbraith@gmail.com>
2021-12-06 14:15:24 -0800

    handler is plugging and chugging 1-9

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,17 @@ import "./App.css";
 import TotalDisplay from "./TotalDisplay";
 import CalcButton from "./CalcButton";
 
-import {addOne} from "../actions/index";
+import {addOne, applyNumber} from "../actions/index";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const handleClick1 = (e) => {
-    dispatch(addOne());
+  // const handleClick1 = () => {
+  //   dispatch(addOne());
+  // }
+
+  const handleClickNum = (e) => {
+    dispatch(applyNumber(Number(e.target.value)));
   }
 
   return (
@@ -37,21 +41,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton onClick={handleClick1} value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton onClick={handleClickNum} value={1}/>
+              <CalcButton onClick={handleClickNum} value={2}/>
+              <CalcButton onClick={handleClickNum} value={3}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton onClick={handleClickNum} value={4}/>
+              <CalcButton onClick={handleClickNum} value={5}/>
+              <CalcButton onClick={handleClickNum} value={6}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton onClick={handleClickNum} value={7}/>
+              <CalcButton onClick={handleClickNum} value={8}/>
+              <CalcButton onClick={handleClickNum} value={9}/>
             </div>
 
             <div className="row">
